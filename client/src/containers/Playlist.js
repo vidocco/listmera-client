a100ff1968e0a89ddc55abcfc625381c00f1833f
vidocco <jack.mk.thibaut@gmail.com>
2017-11-11 09:27:59 +0100

    feat: Playlist page created

--- client/src/containers/Playlist.js
+++ client/src/containers/Playlist.js
@@ -12,7 +12,16 @@ class Playlist extends Component {
     super(props);
     fetch(`http://localhost:3000/api${window.location.pathname}`)
       .then(res => res.json())
-      .then(res => this.setState(res))
+      .then(res => {
+        if (JSON.parse(window.localStorage.getItem('user')).username === res.admin) {
+          this.setState({
+            ...res,
+            isAdmin: true,
+          });
+        } else {
+          this.setState(res);
+        }
+      })
       .catch(e => console.error(e));
   }
 
@@ -20,15 +29,23 @@ class Playlist extends Component {
 
   renderTracks(tracks) {
     return tracks.map((el, i) => {
-      return <Track key={i} img={el.image} title={el.name} artists={el.artist} album={el.album} popularity={el.popularity}/>
+      return <Track key={i} 
+        img={el.image}
+        title={el.name}
+        artists={el.artist}
+        album={el.album}
+        popularity={el.popularity}/>
     })
   }
 
   render() {
     console.log(this.state);
+    let generate = (this.state && this.state.isAdmin)
+      ? (<button className="Generate">GENERATE</button>)
+      : (<button className="Collaborate">COLLABORATE</button>);
     let extra = (this.state && this.state.tracks.length === 50) 
-      ? (<p>{`... and ${this.state.length - this.state.tracks.length} songs more`}</p>)
-      : (<p>this playlist needs a little help, why not collaborate with it?</p>)
+      ? (<p className="MoreSongs">{`... and ${this.state.length - this.state.tracks.length} songs more`}</p>)
+      : (<p className="MoreSongs">this playlist needs a little help, why not collaborate with it?</p>)
     let tracks = this.state ? this.renderTracks(this.state.tracks) : 'waiting';
     let name = this.state ? this.state.name : 'Pending';
     let admin = this.state ? this.state.admin : 'admin';
@@ -36,9 +53,16 @@ class Playlist extends Component {
       <div className="Wrapper">
         <Header />
         <div className="MaxWidthCreate">
-          <h1>{name}</h1>
-          <p>{'created by ' + admin}</p>
-          {tracks}
+          <div className="PlaylistTitleWrapper">
+            <div className="PlaylistTitle">
+              <h1>{name}</h1>
+              <p>{'created by ' + admin}</p>
+            </div>
+            {generate}
+          </div>
+          <div className="TrackWrapper">
+            {tracks}
+          </div>
           {extra}
         </div>
       </div>
