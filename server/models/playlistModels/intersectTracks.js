6e1bbcc57dfe19a8ca56e307144633305729a34c
vidocco <jack.mk.thibaut@gmail.com>
2017-11-13 23:24:27 +0100

    fix: glitch on back-end not storing collabs correctly

--- server/models/playlistModels/intersectTracks.js
+++ server/models/playlistModels/intersectTracks.js
@@ -3,17 +3,19 @@ const client = require('./redis.js');
 async function intersect(playlist, collab, collaborator) {
   return new Promise(async (resolve, reject) => {
     client.sismember(`collabs: ${playlist.collabs}`, collaborator, (err, results) => {
-      if (err) reject(404);
+      console.log(playlist);
+      if (err) reject(500);
       if (results) {
+        console.log('collabed');
         resolve(409);
       } else {
         client.SINTER(`tracks:${playlist.bank}`, `tracks:${collab}`, (err, intersect) => {
           if (intersect.length) client.sadd(`tracks:${playlist.tracks}`, intersect);
-          if (err) reject(404);
+          if (err) reject(500);
           client.sdiff(`tracks:${collab}`, `tracks:${playlist.bank}`, (err, diff) => {
             if (diff.length) client.sadd(`tracks:${playlist.bank}`, diff);
             client.sadd(`collabs: ${playlist.collabs}`, collaborator);
-            if (err) reject(404);
+            if (err) reject(500);
             resolve(200);
           });
         });
--- server/models/playlistModels/retrieveTrackList.js
+++ server/models/playlistModels/retrieveTrackList.js
@@ -3,7 +3,7 @@ const client = require('./redis.js');
 async function retrieveTrackList(id) {
   return new Promise((resolve, reject) => {
     client.hgetall(`playlist:${id}`, async (err, reply) => {
-      resolve({bank: reply.bank, tracks: reply.tracks});
+      resolve({bank: reply.bank, tracks: reply.tracks, collabs: reply.collabs});
       if (err) reject(err);
     });
   });
