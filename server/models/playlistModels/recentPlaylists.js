f0d9f735211e1ffb138c256cc9fa48eabf39b230
vidocco <jack.mk.thibaut@gmail.com>
2017-11-13 23:11:22 +0100

    fix: solved duplicate songs in db bug?

--- server/models/playlistModels/recentPlaylists.js
+++ server/models/playlistModels/recentPlaylists.js
@@ -5,6 +5,7 @@ async function getPlaylist() {
   return new Promise((resolve, reject) => {
     client.smembers('recent', async (err, reply) => {
       if(err) reject(err);
+      if(!reply.length) resolve({playlists: []});
       let playlists = await Promise.all(reply.map(async el => {
         return await display(el, true);
       }));
