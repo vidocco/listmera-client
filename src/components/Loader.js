07c083eab86a6379645496c0d654dad5465dc829
saerose <sbarcelocompte@gmail.com>
2018-09-29 13:40:01 +0200

    feat: changed loader, check later and maybe change again

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,9 +4,10 @@ import '../stylesheets/components/Loader.sass';
 class Loader extends Component {
   render() {
     return (
-      <div className="LoaderWrap">
-        <img className="Loader" src={require('../assets/loading.png')} alt="loading..."/>
-      </div>
+      // <div className="loader_wrap">
+      //   <img className="Loader" src={require('../assets/loading.png')} alt="loading..."/>
+      // </div>
+      <div class="loader">Loading...</div>
     );
   }
 }
